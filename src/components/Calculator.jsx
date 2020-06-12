1814b55115558f3dde1fda563f3ed243235ecfbe
Joseph Kim <josephkim570@gmail.com>
2020-06-12 00:54:05 -0400

    Added changes to the expense functions - relooked at original code and had to understand how it worked that way, it was quite insightful and i think its actually a better idea to keep and build on top of it. This time around, I added some notes to reference for self in the future.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,34 +1,89 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class Calculator extends Component {
     constructor(props) {
         super(props);
         this.state = {
             expense : [
-                { name : '', value: 0 }
+                // value is best left as string rather than 0,
+                // otherwise it adds onto the 0 + 'entry' and 
+                // UX is a pain, having to manually delete before entry
+                { name : '', value: '' }
             ],
             income : [
-                { name : '', value : 0 }
+                { name : '', value : '' }
             ],  
             expenseTotal: 0,
             incomeTotal: 0
         }
-        this.handleChange = this.handleChange.bind(this);
+        
+        // Expense-related
         this.submitExpense = this.submitExpense.bind(this);
-        this.submitIncome = this.submitIncome.bind(this);
         this.addExpenseForm = this.addExpenseForm.bind(this);
+        this.addExpenseState = this.addExpenseState.bind(this);
+
+        // Income-related
+        this.submitIncome = this.submitIncome.bind(this);
+
     }
 
     // Expense-related
 
-    addExpenseForm() {
+    addExpenseState() {
         this.setState(prevState => ({
             expense : [...prevState.expense, 
-                { name : '', value : 0 }
+                { name : '', value : '' }
             ]
         }));
-        // Add styled component of duplicate form for easier reusability
-        // or place the form into a util folder and import here for both
+    };
+
+    deleteExpense(i) {
+        let expense = [...this.state.expense];
+        expense.splice(i, 1);
+        this.setState({ expense });
+    };
+
+    addExpenseForm() {
+        return this.state.expense.map((e, i) => (
+            <div key={i}>
+                {/* Delete form button */}
+                <input
+                    name='delExpForm'
+                    type='button'
+                    value='X'
+                    onClick={this.deleteExpense.bind(this, i)}
+                />
+                <div>Entry #{i}</div>
+                <label/>Name of Expense
+                    <input 
+                        // name must match associated state -- see notes
+                        name='name'
+                        type='text'
+                        placeholder='Name of expense'
+                        // must bind here, because of new instances of i,
+                        // cannot be recognized near the constructor due to scope
+                        onChange={this.expenseChanges.bind(this, i)}
+                        value={e.name}
+                        required
+                    />
+                    <label/>Expense Amount
+                    <input 
+                        name='value'
+                        type='text'
+                        placeholder='Cost of expense'
+                        onChange={this.expenseChanges.bind(this, i)}
+                        value={e.value}
+                        required
+                    />
+            </div>
+        ))
+    }
+
+    expenseChanges(i, e) {
+        let expense = [...this.state.expense];
+        expense[i] = {...expense[i], [e.target.name] : e.target.value};
+        this.setState({ expense });
     };
 
     submitExpense(e) {
@@ -38,6 +93,16 @@ class Calculator extends Component {
         });
     };
 
+    // Income-related
+
+    addIncState() {
+        this.setState(prevState => ({
+            expense : [...prevState.expense, 
+                { name : '', value : 0 }
+            ]
+        }));
+    };
+
     submitIncome(e) {
         e.preventDefault();
         this.setState({
@@ -45,45 +110,29 @@ class Calculator extends Component {
         });
     };
 
-    handleChange(e) {
-        this.setState({
-            [e.target.name] : e.target.value
-        });
-    };
-
     render() { 
         return (  
             <div>
                 This is the calculator component
-
-                <form onSubmit={this.submitExpense}>
-                    <label/>Name of Expense
+               
+                <form>  
+                
                     <input 
-                        name='expenseName'
-                        type='text'
-                        placeholder='Name of expense'
-                        onChange={this.handleChange}
-                        value={this.state.expenseName}
-                    />
-                    <label/>Expense Amount
-                    <input 
-                        name='expenseValue'
-                        type='text'
-                        placeholder='Cost of expense'
-                        onChange={this.handleChange}
-                        value={this.state.expenseValue}
+                        type='button'
+                        onClick={this.addExpenseState}
+                        value='+'
                     />
 
-                    <button onClick={this.addExpenseForm}>+</button>
+                    {this.addExpenseForm()}
 
-                    <input
-                    type='submit'
-                    />
+                    <input type='submit'/>
+
                 </form>
+                
                 <form onSubmit={this.submitIncome}>
                     <label/>Name of Income
                     <input 
-                        name='incomeName'
+                        name='name'
                         type='text'
                         placeholder='Name of Income'
                         onChange={this.handleChange}
@@ -91,7 +140,7 @@ class Calculator extends Component {
                     />
                     <label/>Income Amount
                     <input 
-                        name='incomeValue'
+                        name='value'
                         type='text'
                         placeholder='Name of expense'
                         onChange={this.handleChange}
